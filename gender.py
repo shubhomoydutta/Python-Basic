a52d9d8642ae4eae10e22c49eaac944fdca8e545
shubhomoydutta <shubhomoydutta221@gmail.com>
2026-08-16 11:32:43 +0530

    fourth

---
+++ gender.py
@@ -0,0 +1,7 @@
+gen=input("Please tell me your Gender in (M or F):-")
+if gen=="M" or gen=="m":
+    print("Hello! sir good morning")
+elif gen=="F" or gen=="f":
+    print("Hello! maam good morning")
+else:
+    print("Thank you! good morning")
