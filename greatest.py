45480d19f7b5eb4e0030b32d7c2e686a8775d8d6
shubhomoydutta <shubhomoydutta221@gmail.com>
2026-08-18 12:32:43 +0530

    eight

---
+++ greatest.py
@@ -0,0 +1,8 @@
+a=int(input("Enter your first number :"))
+b=int(input("Enter your second number :"))
+if a>b:
+    print(f"your {a} number is the greatest number")
+elif b>a:
+    print(f"your {b} number is the greatest number")
+else:
+    print("invalid number")
